c0bf904f260a47b85df04a3bc7361f3d691aa549
Julius Wallnöfer <33934646+jwallnoefer@users.noreply.github.com>
2021-10-12 10:51:12 +0200

    removes dejmps from aux_functions

--- src/requsim/libs/aux_functions.py
+++ src/requsim/libs/aux_functions.py
@@ -5,19 +5,6 @@ import pandas as pd
 from warnings import warn
 
 
-sqrt_plus_ix = 1 / np.sqrt(2) * (mat.I(2) + 1j * mat.X)
-sqrt_minus_ix = mat.H(sqrt_plus_ix)
-bilateral_cnot = np.dot(mat.CNOT(0, 2, N=4), mat.CNOT(1, 3, N=4))
-dejmps_operator = np.dot(
-    bilateral_cnot, mat.tensor(sqrt_minus_ix, sqrt_plus_ix, sqrt_minus_ix, sqrt_plus_ix)
-)
-dejmps_operator_dagger = mat.H(dejmps_operator)
-dejmps_proj_ket_z0z0 = mat.tensor(mat.I(4), mat.z0, mat.z0)
-dejmps_proj_ket_z1z1 = mat.tensor(mat.I(4), mat.z1, mat.z1)
-dejmps_proj_bra_z0z0 = mat.H(dejmps_proj_ket_z0z0)
-dejmps_proj_bra_z1z1 = mat.H(dejmps_proj_ket_z1z1)
-
-
 def binary_entropy(p):
     if p == 1 or p == 0:
         return 0
@@ -429,38 +416,5 @@ def w_noise_channel(rho, alpha):
         The density matrix with the map applied.
 
     """
-    return alpha * rho + (1 - alpha) * mat.I(2) / 2 * np.trace(
-        rho
-    )  # trace is necessary if dealing with unnormalized states (e.g. in apply_single_qubit_map)
-
-
-def dejmps_protocol(rho):
-    """Applies the DEJMPS entanglement purification protocol.
-
-    Input is usually two entangled pairs and output is one entangled pair if
-    successful.
-    This protocol was introduced in:
-    D. Deutsch, et. al., Phys. Rev. Lett., vol. 77, pp. 2818–2821 (1996)
-    arXiv:quant-ph/9604039
-
-
-    Parameters
-    ----------
-    rho : np.ndarray
-        Four-qubit density matrix (16x16).
-
-    Returns
-    -------
-    p_suc : scalar
-        probability of success for the protocol
-    state : np.ndarray
-        Two-qubit density matrix (4x4). The state of the remaining pair IF the
-        protocol was successful.
-    """
-    rho = np.dot(np.dot(dejmps_operator, rho), dejmps_operator_dagger)
-    rho = np.dot(np.dot(dejmps_proj_bra_z0z0, rho), dejmps_proj_ket_z0z0) + np.dot(
-        np.dot(dejmps_proj_bra_z1z1, rho), dejmps_proj_ket_z1z1
-    )
-    p_suc = np.trace(rho)
-    state = rho / p_suc  # renormalize
-    return p_suc, state
+    # trace is necessary if dealing with unnormalized states (e.g. in apply_single_qubit_map)
+    return alpha * rho + (1 - alpha) * mat.I(2) / 2 * np.trace(rho)
