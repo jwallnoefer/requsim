b2323b5102c53f96e5938802dc70906bb463c9ca
Julius Wallnöfer <33934646+jwallnoefer@users.noreply.github.com>
2021-11-10 16:34:26 +0100

    adds description of tools package

---
+++ src/requsim/tools/__init__.py
@@ -0,0 +1,9 @@
+"""A module to help working with requsim.
+
+The tools module collects useful ways to set up and interact with requsim
+simulations, but which are not directly tied to the core functionality (and
+is therefore not automatically imported with "import requsim").
+It also includes useful functions for evaluating the results from the simulation
+and a collection of useful pre-defined instances of requsim objects that are
+regularly used, e.g. for error models.
+"""
