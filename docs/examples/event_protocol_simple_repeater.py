aae0a702bc8566315c710b43f45337cbba957cc6
Julius Wallnöfer <33934646+jwallnoefer@users.noreply.github.com>
2021-11-23 17:19:43 +0100

    adds some examples, definitely not finished

---
+++ docs/examples/event_protocol_simple_repeater.py
@@ -0,0 +1,7 @@
+"""Using the event system to run a simple repeater protocol.
+
+This variant accurately uses timing data and collects information about the
+generated long-distance states. The protocol decisions are made at a global
+level, with the protocol looking at the whole state of the world and deciding
+what to do next.
+"""
