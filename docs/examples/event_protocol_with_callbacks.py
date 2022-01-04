9417e3cb3c40119d818faf45c606836891753c3d
Julius Wallnöfer <33934646+jwallnoefer@users.noreply.github.com>
2022-01-04 18:10:09 +0100

    fixes to simple protocol and adds callback example

--- docs/examples/event_protocol_with_callbacks.py
+++ docs/examples/event_protocol_with_callbacks.py
@@ -1,30 +1,14 @@
-"""Using the event system to run a simple repeater protocol.
+"""An alternative way to use the event system.
 
-This variant accurately uses timing data and collects information about the
-generated long-distance states. The protocol decisions are made at a global
-level, with the protocol looking at the whole state of the world and deciding
-what to do next.
+This implements the same protocol as event_protocol_simple_repeater.py with
+a different mechanism: Instead of globally examining the whole status of the
+simulation and deciding from there what events to add to the event_queue, the
+next thing to do after a event is resolved is already attached to the event via
+a callback.
 
-The world setup and protocol can be described as follows:
-
-Stations A and B want to establish entangled pairs via a repeater station at
-the middle point between them.
-The middle station has a mechanism to create entangled pairs and a quantum
-memory (can hold one qubit for each side). The middle station will continously
-try to establish pairs between itself and both A and B. After it has established
-a pair with both A and B, an entanglement swapping operation is performed to
-connect A and B. Repeat until the desired number of pairs has been established.
-
-Error model: The entangled pairs are considered to be perfect upon generation.
-The quantum memories introduce a time-dependent dephasing noise while a qubit
-sits in memory. The Bell measurement to perform the entanglement swapping is
-assumed to be perfect.
-
-For a detailed explanation of this type of protocol see e.g.
-D. Luong, L. Jiang, J. Kim, N. Lütkenhaus; Applied Physics B 122, 96 (2016)
-Preprint: https://arxiv.org/abs/1508.02811
-The protocol in this example corresponds to the 'simultaneous' variant of the
-protocol discussed there, but with a simpler error model.
+While this breaks the neat interpretation of the protocol deciding what to do
+next purely from the current state of the simulation, this way can save time
+especially for complex setups where the current state may be hard to analyze.
 """
 import numpy as np
 import pandas as pd
@@ -49,53 +33,57 @@ def construct_dephasing_noise_channel(dephasing_time):
     return NoiseChannel(n_qubits=1, channel_function=dephasing_noise_func)
 
 
-class SimpleProtocol(TwoLinkProtocol):
-    # TwoLinkProtocol is a helpful abstract class that provides useful
-    # attributes and methods for dealing with and evaluating repeater protocols
-    # consisting of two links (supports multi-mode memories as well).
-    # It needs to be initialized via its `setup` method after world setup
-    # is complete, in order to find all the relevant objects in the world.
-    def check(self):
-        """Check current status and schedule new events.
-
-        This looks globally at the status of the whole `world` and decides
-        which events should be scheduled because of it. It is intended to be
-        called after every change in the `world`.
-        """
-        left_pairs = self._get_left_pairs()
-        num_left_pairs = len(left_pairs)
-        num_left_pairs_scheduled = len(self._left_pairs_scheduled())
+class CallbackProtocol(TwoLinkProtocol):
+    def _check_for_swapping_A(self, event_dict):
         right_pairs = self._get_right_pairs()
-        num_right_pairs = len(right_pairs)
-        num_right_pairs_scheduled = len(self._right_pairs_scheduled())
+        if not right_pairs:
+            return
+        left_pair = event_dict["output_pair"]
+        right_pair = right_pairs[0]
+        ent_swap_event = EntanglementSwappingEvent(
+            time=self.world.event_queue.current_time,
+            pairs=[left_pair, right_pair],
+            station=self.station_central,
+        )
+        ent_swap_event.add_callback(self._after_swapping)
+        self.world.event_queue.add_event(ent_swap_event)
+
+    def _check_for_swapping_B(self, event_dict):
+        left_pairs = self._get_left_pairs()
+        if not left_pairs:
+            return
+        left_pair = left_pairs[0]
+        right_pair = event_dict["output_pair"]
+        ent_swap_event = EntanglementSwappingEvent(
+            time=self.world.event_queue.current_time,
+            pairs=[left_pair, right_pair],
+            station=self.station_central,
+        )
+        self.world.event_queue.add_event(ent_swap_event)
+        ent_swap_event.add_callback(self._after_swapping)
+
+    def _after_swapping(self, event_dict):
         long_distance_pairs = self._get_long_range_pairs()
-
-        # STEP 1: For each link, if there are no pairs established and
-        #         no pairs scheduled: Schedule a pair.
-        if num_left_pairs + num_left_pairs_scheduled == 0:
-            self.source_A.schedule_event()
-        if num_right_pairs + num_right_pairs_scheduled == 0:
-            self.source_B.schedule_event()
-
-        # STEP 2: If both links are present, do entanglement swapping.
-        if num_left_pairs == 1 and num_right_pairs == 1:
-            left_pair = left_pairs[0]
-            right_pair = right_pairs[0]
-            ent_swap_event = EntanglementSwappingEvent(
-                time=self.world.event_queue.current_time,
-                pairs=[left_pair, right_pair],
-                station=self.station_central,
-            )
-            self.world.event_queue.add_event(ent_swap_event)
-
-        # STEP 3: If a long range pair is present, save its data and delete
-        #         the associated objects.
         if long_distance_pairs:
             for pair in long_distance_pairs:
                 self._eval_pair(pair)
                 for qubit in pair.qubits:
                     qubit.destroy()
                 pair.destroy()
+        self.start()
+
+    def start(self):
+        """Start the event chain.
+
+        This needs to be called once to schedule the initial events.
+        """
+        initial_event_A = self.source_A.schedule_event()
+        initial_event_A.add_callback(self._check_for_swapping_A)
+        initial_event_B = self.source_B.schedule_event()
+        initial_event_B.add_callback(self._check_for_swapping_B)
+
+    def check(self):
+        pass
 
 
 def run(length, max_iter, params):
@@ -165,11 +153,11 @@ def run(length, max_iter, params):
         time_distribution=time_distribution,
         state_generation=state_generation,
     )
-    protocol = SimpleProtocol(world=world, communication_speed=C)
+    protocol = CallbackProtocol(world=world, communication_speed=C)
     protocol.setup()
+    protocol.start()
 
     while len(protocol.time_list) < max_iter:
-        protocol.check()
         world.event_queue.resolve_next_event()
     return protocol
 
--- docs/source/examples.rst
+++ docs/source/examples.rst
@@ -18,6 +18,12 @@ Manually running a simple quantum repeater protocol
 Using the event system with a global protocol
 ---------------------------------------------
 
-.. literalinclude:: ../examples/manual_simple_repeater.py
+.. literalinclude:: ../examples/event_protocol_simple_repeater.py
+    :linenos:
+    :caption:
+
+Using the event system with callbacks
+-------------------------------------
+.. literalinclude:: ../examples/event_protocol_with_callbacks.py
     :linenos:
     :caption:
